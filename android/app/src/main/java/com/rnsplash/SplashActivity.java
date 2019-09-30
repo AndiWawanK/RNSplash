ab156b87e2af7541ed510103ad7c78562b9e4569
Andi WawanK <andifirwansyah250@gmail.com>
2019-10-01 05:00:54 +0700

    fix splash icon name

--- android/app/src/main/java/com/rnsplash/SplashActivity.java
+++ android/app/src/main/java/com/rnsplash/SplashActivity.java
@@ -2,14 +2,14 @@ package com.rnsplash;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity{
+public class SplashActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent( packageContex: this, MainActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
